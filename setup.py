89f56600637578e89d6fc646ee8614446fbb9b80
Doraemonzzz <715073608@qq.com>
2022-07-08 12:23:30 +0800

    upload to pypi

--- setup.py
+++ setup.py
@@ -1,22 +1,30 @@
 from setuptools import setup, find_packages
 
 setup(
-  name='taco',
-  version='0.0.0',
-  description='Transformer components',
-  author='Doraemonzzz',
-  author_email='doraemon_zzz@163.com',
-  url='https://github.com/Doraemonzzz/Trev-Xformers',
-  install_requires=[
-      'torch',
-      'einops',
-  ],
-  packages=find_packages(
-    exclude=[
-            "examples",
-            "examples.*",
-            "data",
-            "data.*",
+    name='tacos',
+    version='0.0.0',
+    description='Transformer components',
+    author='Doraemonzzz',
+    author_email='doraemon_zzz@163.com',
+    url='https://github.com/Doraemonzzz/transformer-components',
+    install_requires=[
+        'torch',
+        'einops',
+    ],
+    packages=find_packages(
+        exclude=[
+                "examples",
+                "examples.*",
+                "data",
+                "data.*",
         ]
-    )
+    ),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+
 )
